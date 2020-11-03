a644cd2561b51bfc2862dcee6d7fcdb0b3348d41
THE SHF <imsunhf@gmail.com>
2020-11-03 14:37:52 +0800

    feat(menu): init menu

---
+++ src/views/system/index/page.vue
@@ -0,0 +1,22 @@
+<template>
+  <d2-container class="page">
+    
+  </d2-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  .logo {
+    width: 120px;
+  }
+}
+</style>
---
+++ src/views/system/menu/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="menu-wrap" ref="menuWrap">
+    <el-row :gutter="20">
+      <el-col :span="5" v-for="item in projects" :key="item">
+        <el-card shadow="hover" class="card">
+          <img class="p-icon" :src="`${$baseUrl}image/menu/cli.png`" alt="" />
+          <p>{{ item }}</p>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import CanvasNest from 'canvas-nest.js'
+const config = {
+  color: '0,0,0',
+  pointColor: '255,0,0',
+  opacity: '1',
+  count: 200
+}
+export default {
+  data () {
+    return {
+      projects: ['ff-cli', 'ff-music']
+    }
+  },
+  mounted () {
+    this.cn = new CanvasNest(this.$refs.menuWrap, config)
+  },
+  beforeDestroy () {
+    this.cn.destroy()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-wrap {
+  height: 100%;
+  padding: 10px;
+  .card {
+    text-align: center;
+    background: #fbfcfd;
+    .p-icon {
+      height: 80px;
+    }
+    p {
+      font-size: 18px;
+      color: #1f2d3d;
+    }
+  }
+}
+</style>
